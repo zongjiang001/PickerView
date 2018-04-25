e3123995080adbd46ec20b2a120f25c8c7f3512b
HouGuangYu <554801011@qq.com>
2018-04-25 09:09:07 +0800

    test7

--- app/src/main/java/com/guang/MainActivity.java
+++ app/src/main/java/com/guang/MainActivity.java
@@ -1,19 +1,19 @@
-package com.guang;
+//package com.guang;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.support.annotation.Nullable;
+//import android.support.v7.app.AppCompatActivity;
 
 
-/**
- * Created by Administrator on 2018/4/23.
- */
-
-public class MainActivity extends AppCompatActivity{
-
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+///**
+// * Created by Administrator on 2018/4/23.
+// */
+//
+//public class MainActivity extends AppCompatActivity{
+//
+//    @Override
+//    protected void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
